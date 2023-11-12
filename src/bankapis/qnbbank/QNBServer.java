1a029f39dbba242bd4288ef0f20dab46fe7b31c3
Hossam Ahmed Fouad <57450146+HossamAhmedFouad@users.noreply.github.com>
2023-11-12 19:55:30 +0200

    Added Bank APIS - QNB Bank - HSBC Bank

--- src/bankapis/qnbbank/QNBServer.java
+++ src/bankapis/qnbbank/QNBServer.java
@@ -2,10 +2,13 @@ package bankapis.qnbbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:src/bankapis/qnbbank/application-qnb.properties")
 public class QNBServer {
     public static void main(String[] args) {
         SpringApplication.run(QNBServer.class, args);
+        System.out.println("Running on http://localhost:8002/QNBapi/bank-accounts");
     }
 }
