79b0f1ce905a7e6389337254416ecb308e2496e6
john samy <johnsamyut@gmail.com>
2023-11-13 02:23:02 +0200

    update Bill package and add logic to Electricity,Water and Gas bill

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,10 @@
 import authentication.Authenticator;
+import providers.WalletProvider;
+import services.Bill;
+import services.ElectricityBill;
 
 public class Client {
     public static void main(String[] args) {
-        System.out.println("123456");
+
     }
 }
--- src/services/WaterBill.java
+++ src/services/WaterBill.java
@@ -2,14 +2,39 @@ package services;
 
 import providers.Provider;
 
+import java.util.Scanner;
+
 public class WaterBill implements Bill{
     @Override
     public boolean pay(Provider provider, String code) {
-        return false;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter code:");
+            String userInputCode = scanner.nextLine();
+
+            boolean paymentSuccessful = provider.transfer();
+
+            if (paymentSuccessful) {
+                System.out.println("Water bill paid successfully using " + provider.getClass().getSimpleName());
+                return true;
+            } else {
+                System.out.println("Failed to pay Water bill using " + provider.getClass().getSimpleName());
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred during Bill: " + e.getMessage());
+            return false;
+        }
     }
 
     @Override
     public void generateBill() {
-
+        System.out.println("************ Water Bill ************");
+        System.out.println("Name: John Doe");
+        System.out.println("Address: 123 Main Street, Cityville");
+        System.out.println("Payment Code: ABC123");
+        System.out.println("Details: ");
+        System.out.println("Electricity consumption details go here.");
+        System.out.println("Amount: $100.00");
+        System.out.println("*****************************************");
     }
 }
